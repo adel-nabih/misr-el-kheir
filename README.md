b8c85914e71c5b9472bbdae30a41a3572518f753
adel-nabih <86003399+adel-nabih@users.noreply.github.com>
2025-01-08 16:40:23 +0200

    updated admin dashboard

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # misr-el-kheir
 Misr El Kheir (Egyptian Charity) - Website
+
+Non-profit website with basic features for users such as donations, volunteering, etc.
+Admin dashboard for monitoring donations, volunteers, and programs
--- indexScript.js
+++ indexScript.js
@@ -5,38 +5,34 @@ document.addEventListener("DOMContentLoaded", function () {
     let currentIndex = 0;
     let interval;
 
-    // Function to show a specific slide
     function showSlide(index) {
-        // Hide all slides
+        // hides all slides
         slides.forEach((slide) => slide.classList.remove("active"));
 
-        // Show the current slide
+        // shows only the current slide
         slides[index].classList.add("active");
     }
 
-    // Function to move to the next slide
+    // next slide
     function nextSlide() {
         currentIndex = (currentIndex + 1) % slides.length;
         showSlide(currentIndex);
     }
 
-    // Function to move to the previous slide
+    // previous slide
     function prevSlide() {
         currentIndex = (currentIndex - 1 + slides.length) % slides.length;
         showSlide(currentIndex);
     }
 
-    // Start the automatic slideshow
     function startSlideshow() {
         interval = setInterval(nextSlide, 5000); // Change slide every 5 seconds
     }
 
-    // Stop the automatic slideshow
     function stopSlideshow() {
         clearInterval(interval);
     }
 
-    // Event listeners for manual navigation
     prevBtn.addEventListener("click", () => {
         stopSlideshow();
         prevSlide();
@@ -49,7 +45,6 @@ document.addEventListener("DOMContentLoaded", function () {
         startSlideshow();
     });
 
-    // Initialize the slideshow
     showSlide(currentIndex);
     startSlideshow();
 });
